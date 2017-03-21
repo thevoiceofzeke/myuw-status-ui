58871e0f2d752215f1e819d50f853accf5dfad5d
zeke-witter <david.witter@wisc.edu>
2017-03-21 09:48:53 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>MyUW Status</h1>
+    <h2>Apps</h2>
+    <!-- FOR EACH (grid of status cards) -->
+    <grid-of-cards></grid-of-cards>
+  </div>
+</template>
+
+<script>
+// Get grid component
+import GridOfCards from './components/grid-of-cards.vue'
+
+export default {
+  components: { GridOfCards },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ src/components/grid-of-cards.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'grid-of-cards',
+  data () {
+    return {
+      msg: 'WHAT WHAT'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+p {
+  color: red;
+}
+</style>
